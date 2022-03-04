947dd6fba1435bf1b28aa3fc4c3584623058c2ea
Sid <selormlotsu0@gmail.com>
2022-03-04 17:09:27 +0000

    Update db models

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS users(
     id UUID PRIMARY KEY,
     email VARCHAR(150),
-    user_password VARCHAR(16),
+    user_password VARCHAR(150),
     created_at TIMESTAMP
 );
--- storage/storage_database.go
+++ storage/storage_database.go
@@ -13,24 +13,22 @@ type UserDatabase struct {
 
 func (ub UserDatabase) Save(model *UserModel) error {
 	_, err := ub.Conn.Exec(context.Background(), "insert into users(id, email, user_password, created_at) values($1, $2, $3, $4)",
-		model.ID.String(), model.Email, model.UserPassword, model.CreatedAt)
+		model.ID, model.Email, model.UserPassword, model.CreatedAt)
 	return err
 }
 
 func (ub UserDatabase) Find(id string) (*UserModel, error) {
-	row := ub.Conn.QueryRow(context.Background(), "select * from users where id=$1 or email=$1", id)
-	var uid string
+	row := ub.Conn.QueryRow(context.Background(), "select * from users where id=$1 or email=$2", uuid.FromStringOrNil(id), id)
 	var model UserModel
-	err := row.Scan(&uid, &model.Email, &model.UserPassword, &model.CreatedAt)
+	err := row.Scan(&model.ID, &model.Email, &model.UserPassword, &model.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
-	model.ID = uuid.FromStringOrNil(uid)
 	return &model, nil
 }
 
 func (ub UserDatabase) Delete(id string) error {
-	_, err := ub.Conn.Exec(context.Background(), "delete from users where id=$1", id)
+	_, err := ub.Conn.Exec(context.Background(), "delete from users where id=$1", uuid.FromStringOrNil(id))
 	return err
 }
 
@@ -39,13 +37,13 @@ type CompanyDatabase struct {
 }
 
 func (cb CompanyDatabase) Save(model *CompanyModel) error {
-	row := cb.Conn.QueryRow(context.Background(), "select * from companies where id=$1", model.ID.String())
+	row := cb.Conn.QueryRow(context.Background(), "select * from companies where id=$1", model.ID)
 	if row.Scan() == pgx.ErrNoRows {
 		_, err := cb.Conn.Exec(context.Background(), "insert into companies(id, company_user_id, company_name, email, logo, created_at) values($1, $2, $3, $4, $5, $6)",
-			model.ID.String(), model.CompanyUserID, model.CompanyName, model.Email, model.Logo, model.CreatedAt)
+			model.ID, model.CompanyUserID, model.CompanyName, model.Email, model.Logo, model.CreatedAt)
 		return err
 	}
-	_, err := cb.Conn.Exec(context.Background(), "update companies set company_name=$2, email=$3, logo=$4 where id=$1", model.ID.String(), model.CompanyName, model.Email, model.Logo)
+	_, err := cb.Conn.Exec(context.Background(), "update companies set company_name=$2, email=$3, logo=$4 where id=$1", model.ID, model.CompanyName, model.Email, model.Logo)
 	return err
 }
 
@@ -56,32 +54,28 @@ func (cb CompanyDatabase) List() ([]CompanyModel, error) {
 	}
 	var models []CompanyModel
 	for rows.Next() {
-		var id string
 		var model CompanyModel
-		err = rows.Scan(&id, &model.CompanyUserID, &model.CompanyName, &model.Email, &model.Logo, &model.CreatedAt)
+		err = rows.Scan(&model.ID, &model.CompanyUserID, &model.CompanyName, &model.Email, &model.Logo, &model.CreatedAt)
 		if err != nil {
 			return nil, err
 		}
-		model.ID = uuid.FromStringOrNil(id)
 		models = append(models, model)
 	}
 	return models, nil
 }
 
 func (cb CompanyDatabase) Find(id string) (*CompanyModel, error) {
-	row := cb.Conn.QueryRow(context.Background(), "select * from companies where id=$1 or company_user_id=$1", id)
-	var uid string
+	row := cb.Conn.QueryRow(context.Background(), "select * from companies where id=$1 or company_user_id=$2", uuid.FromStringOrNil(id), id)
 	var model CompanyModel
-	err := row.Scan(&uid, &model.CompanyUserID, &model.CompanyName, &model.Email, &model.Logo, &model.CreatedAt)
+	err := row.Scan(&model.ID, &model.CompanyUserID, &model.CompanyName, &model.Email, &model.Logo, &model.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
-	model.ID = uuid.FromStringOrNil(uid)
 	return &model, nil
 }
 
 func (cb CompanyDatabase) Delete(id string) error {
-	_, err := cb.Conn.Exec(context.Background(), "delete from companies where id=$1", id)
+	_, err := cb.Conn.Exec(context.Background(), "delete from companies where id=$1", uuid.FromStringOrNil(id))
 	return err
 }
 
@@ -90,13 +84,13 @@ type ProductDatabase struct {
 }
 
 func (cb ProductDatabase) Save(model *ProductModel) error {
-	row := cb.Conn.QueryRow(context.Background(), "select * from products where id=$1", model.ID.String())
+	row := cb.Conn.QueryRow(context.Background(), "select * from products where id=$1", model.ID)
 	if row.Scan() == pgx.ErrNoRows {
 		_, err := cb.Conn.Exec(context.Background(), "insert into products(id, company_id, product_name, feedback_url, rating, created_at) values($1, $2, $3, $4, $5, $6)",
-			model.ID.String(), model.CompanyID.String(), model.ProductName, model.FeedbackURL, model.Rating, model.CreatedAt)
+			model.ID, model.CompanyID, model.ProductName, model.FeedbackURL, model.Rating, model.CreatedAt)
 		return err
 	}
-	_, err := cb.Conn.Exec(context.Background(), "update products set product_name=$2, feedback_url=$3, rating=$4 where id=$1", model.ID.String(), model.ProductName, model.FeedbackURL, model.Rating)
+	_, err := cb.Conn.Exec(context.Background(), "update products set product_name=$2, feedback_url=$3, rating=$4 where id=$1", model.ID, model.ProductName, model.FeedbackURL, model.Rating)
 	return err
 }
 
@@ -107,36 +101,28 @@ func (cb ProductDatabase) List() ([]ProductModel, error) {
 	}
 	var models []ProductModel
 	for rows.Next() {
-		var id string
-		var companyID string
 		var model ProductModel
-		err = rows.Scan(&id, &companyID, &model.ProductName, &model.FeedbackURL, &model.Rating, &model.CreatedAt)
+		err = rows.Scan(&model.ID, &model.CompanyID, &model.ProductName, &model.FeedbackURL, &model.Rating, &model.CreatedAt)
 		if err != nil {
 			return nil, err
 		}
-		model.ID = uuid.FromStringOrNil(id)
-		model.CompanyID = uuid.FromStringOrNil(companyID)
 		models = append(models, model)
 	}
 	return models, nil
 }
 
 func (cb ProductDatabase) Find(id string) (*ProductModel, error) {
-	row := cb.Conn.QueryRow(context.Background(), "select * from products where id=$1", id)
-	var uid string
-	var companyID string
+	row := cb.Conn.QueryRow(context.Background(), "select * from products where id=$1", uuid.FromStringOrNil(id))
 	var model ProductModel
-	err := row.Scan(&uid, &companyID, &model.ProductName, &model.FeedbackURL, &model.Rating, &model.CreatedAt)
+	err := row.Scan(&model.ID, &model.CompanyID, &model.ProductName, &model.FeedbackURL, &model.Rating, &model.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
-	model.ID = uuid.FromStringOrNil(uid)
-	model.CompanyID = uuid.FromStringOrNil(companyID)
 	return &model, nil
 }
 
 func (cb ProductDatabase) Delete(id string) error {
-	_, err := cb.Conn.Exec(context.Background(), "delete from products where id=$1", id)
+	_, err := cb.Conn.Exec(context.Background(), "delete from products where id=$1", uuid.FromStringOrNil(id))
 	return err
 }
 
@@ -145,13 +131,13 @@ type ReviewDatabase struct {
 }
 
 func (cb ReviewDatabase) Save(model *ReviewModel) error {
-	row := cb.Conn.QueryRow(context.Background(), "select * from reviews where id=$1", model.ID.String())
+	row := cb.Conn.QueryRow(context.Background(), "select * from reviews where id=$1", model.ID)
 	if row.Scan() == pgx.ErrNoRows {
 		_, err := cb.Conn.Exec(context.Background(), "insert into reviews(id, company_id, product_id, comment, rating, created_at) values($1, $2, $3, $4, $5, $6)",
-			model.ID.String(), model.CompanyID.String(), model.ProductID.String(), model.Comment, model.Rating, model.CreatedAt)
+			model.ID, model.CompanyID, model.ProductID, model.Comment, model.Rating, model.CreatedAt)
 		return err
 	}
-	_, err := cb.Conn.Exec(context.Background(), "update reviews set comment=$2, rating=$3 where id=$1", model.ID.String(), model.Comment, model.Rating)
+	_, err := cb.Conn.Exec(context.Background(), "update reviews set comment=$2, rating=$3 where id=$1", model.ID, model.Comment, model.Rating)
 	return err
 }
 
@@ -162,39 +148,27 @@ func (cb ReviewDatabase) List() ([]ReviewModel, error) {
 	}
 	var models []ReviewModel
 	for rows.Next() {
-		var id string
-		var companyID string
-		var productID string
 		var model ReviewModel
-		err = rows.Scan(&id, &companyID, &productID, &model.Comment, &model.Rating, &model.CreatedAt)
+		err = rows.Scan(&model.ID, &model.CompanyID, &model.ProductID, &model.Comment, &model.Rating, &model.CreatedAt)
 		if err != nil {
 			return nil, err
 		}
-		model.ID = uuid.FromStringOrNil(id)
-		model.CompanyID = uuid.FromStringOrNil(companyID)
-		model.ProductID = uuid.FromStringOrNil(productID)
 		models = append(models, model)
 	}
 	return models, nil
 }
 
 func (cb ReviewDatabase) Find(id string) (*ReviewModel, error) {
-	row := cb.Conn.QueryRow(context.Background(), "select * from reviews where id=$1", id)
-	var uid string
-	var companyID string
-	var productID string
+	row := cb.Conn.QueryRow(context.Background(), "select * from reviews where id=$1", uuid.FromStringOrNil(id))
 	var model ReviewModel
-	err := row.Scan(&uid, &companyID, &productID, &model.Comment, &model.Rating, &model.CreatedAt)
+	err := row.Scan(&model.ID, &model.CompanyID, &model.ProductID, &model.Comment, &model.Rating, &model.CreatedAt)
 	if err != nil {
 		return nil, err
 	}
-	model.ID = uuid.FromStringOrNil(uid)
-	model.CompanyID = uuid.FromStringOrNil(companyID)
-	model.ProductID = uuid.FromStringOrNil(productID)
 	return &model, nil
 }
 
 func (cb ReviewDatabase) Delete(id string) error {
-	_, err := cb.Conn.Exec(context.Background(), "delete from reviews where id=$1", id)
+	_, err := cb.Conn.Exec(context.Background(), "delete from reviews where id=$1", uuid.FromStringOrNil(id))
 	return err
 }
